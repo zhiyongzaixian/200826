6f60946ad940d6148f5b6b62c472f533e53defb1
zhiyongzaixian <13852292775@163.com>
2021-01-26 14:24:49 +0800

    '0126'

--- guiguMusic_study/pages/login/login.js
+++ guiguMusic_study/pages/login/login.js
@@ -13,6 +13,8 @@
       5) 密码正确，返回登录成功数据
 
 */
+
+import request from '../../utils/request.js'
 Page({
 
   /**
@@ -27,7 +29,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    
   },
 
   // 表单项事件的回调
@@ -42,6 +44,79 @@ Page({
     // let a = 'name'
     // let obj = {}
     // obj[a] = 'xxx'
+  },
+
+  // 登陆的回调
+  async login(){
+    //1. 收集表单项数据 
+    let {phone, password} = this.data;
+
+    // 2. 前端验证
+    /*
+      手机号验证
+        1. 内容为空
+        2. 手机号格式不正确
+        3. 手机号格式正确，验证通过
+    */ 
+
+    if(!phone){ // 1. 内容为空
+      // alert('手机号不能为空');
+      // 提示用户
+      wx.showToast({
+        title: '手机号不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+
+    // 定义正则表达式
+    let phoneReg = /^1(3|4|5|6|7|8|9)\d{9}$/;
+    if(!phoneReg.test(phone)){ // 2. 手机号格式不正确
+       // 提示用户
+       wx.showToast({
+        title: '手机号格式错误',
+        icon: 'none'
+      })
+      return;
+    }
+
+    // 验证密码
+    if(!password){
+      // 提示用户
+      wx.showToast({
+        title: '密码不能为空',
+        icon: 'none'
+      })
+      return;
+    }
+
+    // 后端验证
+    let result = await request('/login/cellphone', {phone, password}); 
+    // 200, 400, 502
+    if(result.code === 200){
+      // 登录成功
+      wx.showToast({
+        title: '登录成功'
+      })
+    }else if(result.code === 400){
+      wx.showToast({
+        title: '手机号错误',
+        icon: 'none'
+      })
+    }else if(result.code === 502){
+      wx.showToast({
+        title: '密码错误',
+        icon: 'none'
+      })
+    }else {
+      wx.showToast({
+        title: '登录失败，请重新登录',
+        icon: 'none'
+      })
+    }
+    
+
+
   },
 
   /**
