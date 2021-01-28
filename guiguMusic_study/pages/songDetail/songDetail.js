74145bed19ea9b6cd5a5968eb72cf981fe78fc7b
zhiyongzaixian <13852292775@163.com>
2021-01-28 10:56:26 +0800

    '1028'

--- guiguMusic_study/pages/songDetail/songDetail.js
+++ guiguMusic_study/pages/songDetail/songDetail.js
@@ -1,36 +1,18 @@
-import request from '../../utils/request'
+// pages/songDetail/songDetail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    day: '',
-    month: '',
-    recommendList: [], // 每日推荐数据
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // 动态获取当前日期
-    this.setData({
-      day: new Date().getDate(),
-      month: new Date().getMonth() + 1
-    })
 
-    this.getRecommendList();
-  },
-
-  // 获取每日推荐recommendList数据的功能函数
-  async getRecommendList(){
-    let recommendListData = await request('/recommend/songs');
-
-    // 更新状态
-    this.setData({
-      recommendList: recommendListData.recommend
-    })
   },
 
   /**
