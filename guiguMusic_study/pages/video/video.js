9f4e0345a1dd659fb758141e57cdaefc0b889f13
zhiyongzaixian <13852292775@163.com>
2021-01-28 09:03:56 +0800

    '0128'

--- guiguMusic_study/pages/video/video.js
+++ guiguMusic_study/pages/video/video.js
@@ -1430,7 +1430,21 @@ Page({
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-
+  onShareAppMessage: function ({from}) {
+    console.log(from)
+    if(from === 'menu'){
+        return {
+            title: '这是menu的内容',
+            path: '/pages/video/video',
+            imageUrl: '/static/images/nvsheng.jpg'
+        }
+    }else {
+        return {
+            title: '这是button的内容',
+            path: '/pages/video/video',
+            imageUrl: '/static/images/nvsheng.jpg'
+        }
+    }
+   
   }
 })
