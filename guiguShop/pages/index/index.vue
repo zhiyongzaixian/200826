88d0ef4798fb23a3e5c9606ac7f260c1963d95e8
zhiyongzaixian <13852292775@163.com>
2021-02-01 14:20:07 +0800

    '0201'

--- guiguShop/pages/index/index.vue
+++ guiguShop/pages/index/index.vue
@@ -4,10 +4,44 @@
 		<view class="header">
 			<image class="logo" src="/static/images/logo.png" mode=""></image>
 			<view class="search">
+				<text class="iconfont icon-sousuo"></text>
 				<input type="text" value="" placeholder="搜索商品" placeholder-class='placeholder'/>
 			</view>
 			<button>北方汉子</button>
 		</view>
+	
+		<!-- 导航区域 -->
+		<scroll-view scroll-x="true" class="navScroll" enable-flex="true">
+			<view class="navItem active">
+				推荐
+			</view>
+			<view class="navItem">
+				居家生活
+			</view>
+			<view class="navItem">
+				推荐
+			</view>
+			<view class="navItem">
+				居家生活
+			</view>
+			<view class="navItem">
+				推荐
+			</view>
+			<view class="navItem">
+				居家生活
+			</view>
+			<view class="navItem">
+				推荐
+			</view>
+			<view class="navItem">
+				居家生活
+			</view><view class="navItem">
+				推荐
+			</view>
+			<view class="navItem">
+				居家生活
+			</view>
+		</scroll-view>
 	</view>
 </template>
 
@@ -49,10 +83,19 @@
 				height 40rpx
 				margin 10rpx 20rpx
 			.search
+				position relative
 				width 420rpx
 				height 60rpx
 				background #eee
+				.iconfont
+					position absolute
+					font-size 30rpx
+					top 15rpx
+					left 10rpx
 				input
+					width 370rpx
+					margin-left 50rpx
+					height 60rpx
 					.placeholder
 						font-size 26rpx
 					
@@ -64,5 +107,22 @@
 				font-size 24rpx
 				padding 0 4rpx
 				margin 0 10rpx
-			
+		.navScroll
+			// display flex
+			white-space nowrap
+			.navItem
+				display inline-block
+				width 140rpx
+				height 80rpx
+				text-align center
+				line-height 80rpx
+				font-size 26rpx
+				/* 父级引用， &代表当前位置的父级 */
+				&.active
+					border-bottom 1rpx solid #BB2C08
+				
+	
+	
+.test
+	font-size 0
 </style>
